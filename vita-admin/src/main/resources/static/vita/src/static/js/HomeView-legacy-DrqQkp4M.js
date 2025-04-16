85a19a40a3d6385c44ab199ae2149205370b074e
mengweijin <1002284406@qq.com>
2025-04-16 20:59:34 +0800

    ui

--- vita-admin/src/main/resources/static/vita/src/static/js/HomeView-legacy-DrqQkp4M.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-BUpXkeVg.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e._,r=e.c,i=e.o}],execute:function(){e("default",n({},[["render",function(e,t){return i(),r("div",null,"This is Home View!")}]]))}}}));
