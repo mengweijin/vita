ebfb2d2ace97beba397d10552ea7673ed24c70b5
mengweijin <1002284406@qq.com>
2026-04-25 11:13:17 +0800

    format code

--- vita-admin/src/main/java/com/github/mengweijin/vita/VitaAdminApplication.java
+++ vita-admin/src/main/java/com/github/mengweijin/vita/VitaAdminApplication.java
@@ -9,27 +9,28 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * 注意 1：SDK 化引入时，@ComponentScan 和 @MapperScan 需要增加你自己工程的包扫描路径。
  * 注意 2：@MapperScan 路径需要限制只能扫描 mapper 目录下的文件。
+ *
  * @author mengweijin
  */
 @Slf4j
-@ComponentScan(basePackages = { "com.github.mengweijin.vita" })
-@MapperScan(basePackages = { "com.github.mengweijin.vita.**.mapper" })
+@ComponentScan(basePackages = {"com.github.mengweijin.vita"})
+@MapperScan(basePackages = {"com.github.mengweijin.vita.**.mapper"})
 @SpringBootApplication
 public class VitaAdminApplication {
 
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        try {
             long start = System.currentTimeMillis();
-			SpringApplication.run(VitaAdminApplication.class, args);
+            SpringApplication.run(VitaAdminApplication.class, args);
             long end = System.currentTimeMillis();
             log.info("-----------------------------------------------------------------------");
             log.info("----------- Started Vita in {} seconds", (end - start) / 1000D);
             log.info("----------- Vita startup success!");
             log.info("-----------------------------------------------------------------------");
-		} catch (Exception e) {
+        } catch (Exception e) {
             log.error("----------------------------------------------------------------------");
             log.error("---------- Vita startup failed!");
             log.error("----------------------------------------------------------------------");
-		}
-	}
+        }
+    }
 }
